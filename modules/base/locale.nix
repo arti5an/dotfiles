bd14966d3ac14be5b8bf9f0b9a5c2cdbcfab2177
Richard Smith <artis4n@outlook.com>
2024-09-16 17:08:55 +0100

    Make locale spec more complete
    Based on default GUI install of NixOS

--- modules/base/locale.nix
+++ modules/base/locale.nix
@@ -2,10 +2,25 @@
   config,
   lib,
   ...
-}: {
+}: let
+  locale = "en_GB.UTF-8";
+in {
   config = {
     console.keyMap = lib.mkDefault "uk";
 
-    i18n.defaultLocale = lib.mkDefault "en_GB.UTF-8";
+    i18n = {
+      defaultLocale = lib.mkDefault locale;
+      extraLocaleSettings = {
+        LC_ADDRESS = locale;
+        LC_IDENTIFICATION = locale;
+        LC_MEASUREMENT = locale;
+        LC_MONETARY = locale;
+        LC_NAME = locale;
+        LC_NUMERIC = locale;
+        LC_PAPER = locale;
+        LC_TELEPHONE = locale;
+        LC_TIME = locale;
+      };
+    };
   };
 }
